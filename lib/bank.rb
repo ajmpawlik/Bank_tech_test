077437958cf6f01c9c2d40f8429126faf345abc8
Aleksandra <ajmpawlik@gmail.com>
2019-07-29 15:53:34 +0100

    feat: passes the test on response to make_witdrawal

--- lib/bank.rb
+++ lib/bank.rb
@@ -3,7 +3,12 @@ class Bank
   def initialize
     @balance = 0
   end
+
   def make_deposit(amount_deposited)
     @balance = @balance + amount_deposited
+  end
+
+  def make_withdrawal(amount_withdrawed)
+    @balance = @balance - amount_withdrawed
   end
 end
