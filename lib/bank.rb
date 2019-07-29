8559f55ab7e3881e1b35c9850b2a54629548add2
Aleksandra <ajmpawlik@gmail.com>
2019-07-29 14:33:26 +0100

    feat: passing the test on showing balance of the account when set to 0"

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,6 +1,10 @@
 class Bank
+  def initialize
+    @balance = 0
+  end
   def make_deposit
   end
   def balance
+    @balance
   end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -7,7 +7,7 @@ describe Bank do
   it 'responds to method #balance' do
     expect(subject).to respond_to :balance
   end
-  it 'shows the balance of the account'
+  it 'shows the balance of the account' do
     expect(subject.balance).to eq 0
   end
 end
