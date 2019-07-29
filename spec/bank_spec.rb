b329835be9f5099c1d87661772b90860e1e04ef1
Aleksandra <ajmpawlik@gmail.com>
2019-07-29 14:22:02 +0100

    test: the instance of the class Bank responds to the method balance showing the balance of the account

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -4,4 +4,7 @@ describe Bank do
   it 'responds to method #make_deposit' do
     expect(subject).to respond_to :make_deposit
   end
+  it 'responds to method #balance' do
+    expect(subject).to respond_to :balance 
+  end
 end
