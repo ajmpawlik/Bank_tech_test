42bc4d55b8487ba3b017787dc1143787842424b6
Aleksandra <ajmpawlik@gmail.com>
2019-07-29 14:47:38 +0100

    refactor: tests grouped by methods

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,13 +1,17 @@
 require 'bank'
 
 describe Bank do
-  it 'responds to method #make_deposit' do
-    expect(subject).to respond_to :make_deposit
+  describe '#make_deposit' do
+    it 'responds to method #make_deposit' do
+      expect(subject).to respond_to :make_deposit
+    end
   end
-  it 'responds to method #balance' do
-    expect(subject).to respond_to :balance
-  end
-  it 'shows the balance of the account' do
-    expect(subject.balance).to eq 0
+  describe '#balance' do
+    it 'responds to method #balance' do
+      expect(subject).to respond_to :balance
+    end
+    it 'shows the balance of the account' do
+      expect(subject.balance).to eq 0
+    end
   end
 end
