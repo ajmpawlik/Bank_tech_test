4adafbb05bb8920df59b2c0f62a33bead83b5810
Aleksandra <ajmpawlik@gmail.com>
2019-07-30 09:49:32 +0100

    feat: test passing transaction method implemented with the attribute reader

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,14 +1,15 @@
 class Bank
-  attr_reader :balance, :account
+  attr_reader :balance, :account, :transaction
   def initialize
     @balance = 0
     @account = []
-
+    @transaction = {}
   end
 
   def make_deposit(amount_deposited)
     @balance = @balance + amount_deposited
-    @account("deposit", amount_deposited)
+    @account.push(@transaction[:deposit] = amount_deposited)
+    p @account
   end
 
   def make_withdrawal(amount_withdrawed)
